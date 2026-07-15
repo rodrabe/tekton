8d486d6b78ff48033a9a391cb227f56b62aba704
rodrabe <rodrabe@us.ibm.com>
2026-07-15 14:04:26 -0500

    fix: use ibm-ubuntu-22-04-5-minimal-amd64-16 (available in staging)

--- .tekton/deploy.sh
+++ .tekton/deploy.sh
@@ -140,7 +140,7 @@ source "ibmcloud-vpc" "base" {
   iam_url           = "https://iam.test.cloud.ibm.com"
   vpc_endpoint_url  = "https://us-south-stage01.iaasdev.cloud.ibm.com/v1"
 
-  vsi_base_image_name = "ibm-ubuntu-22-04-minimal-amd64-2"
+  vsi_base_image_name = "ibm-ubuntu-22-04-5-minimal-amd64-16"
   vsi_profile         = "bx2-2x8"
   vsi_interface       = "public"
   image_name          = local.full_image
